1586b44d9d44f42c57198e9f0499de6e2e284515
Jitter Juice Joe <jitterjuicejoe@proton.me>
2023-01-25 08:25:06 +0100

    Adjust module layout

--- backend/src/drivers/rppal.rs
+++ backend/src/drivers/rppal.rs
@@ -1,124 +1,133 @@
 /// A module to interface Raspberry Pi peripherals using the `rppal` crate.
 ///
-use crate::{
-    drivers::{gpio, spi},
-    error::Error,
-};
 
-/// A GPIO output pin.
-pub struct GpioOutputPin {
-    /// The `rppal` output pin.
-    pin: rppal::gpio::OutputPin,
-}
+pub mod gpio {
 
-impl GpioOutputPin {
-    /// Returns a [GpioOutputPin] instance.
-    ///
-    /// # Arguments
-    ///
-    /// * `pin` - the number of the pin to use
-    pub fn new(pin: u8) -> Result<Self, Error> {
-        match rppal::gpio::Gpio::new() {
-            Err(err) => Err(Error::new_with_source(
-                "Failed to acquire rppal Gpio instance.",
-                err,
-            )),
-            Ok(instance) => match instance.get(pin) {
+    use crate::drivers::gpio as dgpio;
+    use crate::error::Error;
+    use rppal::gpio as rgpio;
+
+    /// A GPIO output pin.
+    pub struct GpioOutputPin {
+        /// The `rppal` output pin.
+        pin: rgpio::OutputPin,
+    }
+
+    impl GpioOutputPin {
+        /// Returns a [GpioOutputPin] instance.
+        ///
+        /// # Arguments
+        ///
+        /// * `pin` - the number of the pin to use
+        pub fn new(pin: u8) -> Result<Self, Error> {
+            match rgpio::Gpio::new() {
                 Err(err) => Err(Error::new_with_source(
-                    &format!("Failed to acquire pin {}", pin),
+                    "Failed to acquire rppal Gpio instance.",
                     err,
                 )),
-                Ok(pin) => Ok(Self {
-                    pin: pin.into_output(),
-                }),
-            },
+                Ok(instance) => match instance.get(pin) {
+                    Err(err) => Err(Error::new_with_source(
+                        &format!("Failed to acquire pin {}", pin),
+                        err,
+                    )),
+                    Ok(pin) => Ok(Self {
+                        pin: pin.into_output(),
+                    }),
+                },
+            }
+        }
+    }
+
+    impl dgpio::OutputPin for GpioOutputPin {
+        fn set(&mut self, state: dgpio::State) {
+            match state {
+                dgpio::State::High => self.pin.set_high(),
+                dgpio::State::Low => self.pin.set_low(),
+            }
         }
     }
 }
 
-impl gpio::OutputPin for GpioOutputPin {
-    fn set(&mut self, state: gpio::State) {
-        match state {
-            gpio::State::High => self.pin.set_high(),
-            gpio::State::Low => self.pin.set_low(),
+pub mod spi {
+
+    use crate::drivers::spi as dspi;
+    use crate::error::Error;
+    use rppal::spi as rspi;
+    /// Raspberry Pi SPI Bus configurations.
+    pub enum Bus {
+        Spi0,
+        Spi1,
+        Spi2,
+        Spi3,
+        Spi4,
+        Spi5,
+        Spi6,
+    }
+
+    /// Chip select pins
+    pub enum Ce {
+        Ce0,
+        Ce1,
+        Ce2,
+    }
+
+    /// A SP Interface.
+    pub struct Spi {
+        /// The `rppal` SPI
+        spi: rspi::Spi,
+    }
+
+    impl Spi {
+        /// Returns a [Spi] instance.
+        ///
+        /// # Arguments
+        ///
+        /// * `bus` - SPI bus to use
+        /// * `ce` - Chip enable pin to use
+        /// * `mode` - SPI mode
+        /// * `freq` - clock frequency
+        ///
+        /// # See Also
+        ///
+        /// * [pinout.xyz](https://pinout.xyz/)
+        pub fn new(bus: Bus, ce: Ce, mode: dspi::Mode, freq: u32) -> Result<Self, Error> {
+            let bus = match bus {
+                Bus::Spi0 => rppal::spi::Bus::Spi0,
+                Bus::Spi1 => rppal::spi::Bus::Spi1,
+                Bus::Spi2 => rppal::spi::Bus::Spi2,
+                Bus::Spi3 => rppal::spi::Bus::Spi3,
+                Bus::Spi4 => rppal::spi::Bus::Spi4,
+                Bus::Spi5 => rppal::spi::Bus::Spi5,
+                Bus::Spi6 => rppal::spi::Bus::Spi6,
+            };
+            let ce = match ce {
+                Ce::Ce0 => rppal::spi::SlaveSelect::Ss0,
+                Ce::Ce1 => rppal::spi::SlaveSelect::Ss1,
+                Ce::Ce2 => rppal::spi::SlaveSelect::Ss2,
+            };
+            let mode = match mode {
+                dspi::Mode::Mode0 => rspi::Mode::Mode0,
+                dspi::Mode::Mode1 => rspi::Mode::Mode1,
+                dspi::Mode::Mode2 => rspi::Mode::Mode2,
+                dspi::Mode::Mode3 => rspi::Mode::Mode3,
+            };
+            match rspi::Spi::new(bus, ce, freq, mode) {
+                Err(err) => Err(Error::new_with_source(
+                    "Failed to acquire rppal SPI instance.",
+                    err,
+                )),
+                Ok(spi) => Ok(Self { spi }),
+            }
         }
     }
-}
 
-/// Raspberry Pi SPI Bus configurations.
-pub enum Bus {
-    Spi0,
-    Spi1,
-    Spi2,
-    Spi3,
-    Spi4,
-    Spi5,
-    Spi6,
-}
-
-/// Chip select pins
-pub enum Ce {
-    Ce0,
-    Ce1,
-    Ce2,
-}
-
-/// A SP Interface.
-pub struct Spi {
-    /// The `rppal` SPI
-    spi: rppal::spi::Spi,
-}
-
-impl Spi {
-    /// Returns a [Spi] instance.
-    ///
-    /// # Arguments
-    ///
-    /// * `bus` - SPI bus to use
-    /// * `ce` - Chip enable pin to use
-    /// * `mode` - SPI mode
-    /// * `freq` - clock frequency
-    ///
-    /// # See Also
-    ///
-    /// * [pinout.xyz](https://pinout.xyz/)
-    pub fn new(bus: Bus, ce: Ce, mode: spi::Mode, freq: u32) -> Result<Self, Error> {
-        let bus = match bus {
-            Bus::Spi0 => rppal::spi::Bus::Spi0,
-            Bus::Spi1 => rppal::spi::Bus::Spi1,
-            Bus::Spi2 => rppal::spi::Bus::Spi2,
-            Bus::Spi3 => rppal::spi::Bus::Spi3,
-            Bus::Spi4 => rppal::spi::Bus::Spi4,
-            Bus::Spi5 => rppal::spi::Bus::Spi5,
-            Bus::Spi6 => rppal::spi::Bus::Spi6,
-        };
-        let ce = match ce {
-            Ce::Ce0 => rppal::spi::SlaveSelect::Ss0,
-            Ce::Ce1 => rppal::spi::SlaveSelect::Ss1,
-            Ce::Ce2 => rppal::spi::SlaveSelect::Ss2,
-        };
-        let mode = match mode {
-            spi::Mode::Mode0 => rppal::spi::Mode::Mode0,
-            spi::Mode::Mode1 => rppal::spi::Mode::Mode1,
-            spi::Mode::Mode2 => rppal::spi::Mode::Mode2,
-            spi::Mode::Mode3 => rppal::spi::Mode::Mode3,
-        };
-        match rppal::spi::Spi::new(bus, ce, freq, mode) {
-            Err(err) => Err(Error::new_with_source(
-                "Failed to acquire rppal SPI instance.",
-                err,
-            )),
-            Ok(spi) => Ok(Self { spi }),
-        }
-    }
-}
-
-impl spi::Spi for Spi {
-    fn read(&mut self, n: usize) -> Result<Vec<u8>, Error> {
-        let mut buf = vec![0; n];
-        match self.spi.read(&mut buf) {
-            Ok(_) => Ok(buf),
-            Err(err) => Err(Error::new_with_source("Failed to read from SPI.", err)),
+    impl dspi::Spi for Spi {
+        fn read(&mut self, n: usize) -> Result<Vec<u8>, Error> {
+            let mut buf = vec![0; n];
+            match self.spi.read(&mut buf) {
+                Ok(_) => Ok(buf),
+                Err(err) => Err(Error::new_with_source("Failed to read from SPI.", err)),
+            }
         }
     }
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,13 +3,13 @@ use std::thread;
 use std::time::Duration;
 
 //use librew::gpio::OutputPin;
-use librew::drivers::rppal;
-use librew::drivers::{spi, spi::Spi};
+use librew::drivers::rppal::spi as rspi;
+use librew::drivers::spi as dspi;
 // Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut spi: Box<dyn Spi> =
-        match rppal::Spi::new(rppal::Bus::Spi0, rppal::Ce::Ce0, spi::Mode::Mode3, 4300000) {
+    let mut spi: Box<dyn dspi::Spi> =
+        match rspi::Spi::new(rspi::Bus::Spi0, rspi::Ce::Ce0, dspi::Mode::Mode1, 4300000) {
             Err(err) => return Err(Box::new(err)),
             Ok(spi) => Box::new(spi),
         };
